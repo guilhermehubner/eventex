17bb2641da6ef14214ad2ae5c299ed3dd6ea479b
Guilherme <guilherme_hubner@msn.com>
2015-12-30 18:01:19 -0200

    Tela de inscrição

---
+++ eventex/subscriptions/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class SubscriptionForm(forms.Form):
+    name = forms.CharField(label='Nome')
+    cpf = forms.CharField(label='CPF')
+    phone = forms.CharField(label='Telefone')
+    email = forms.EmailField(label='Email')
---
+++ eventex/subscriptions/tests.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
+
+
+class SubscriptionsTests(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/inscricao/')
+
+    def test_get(self):
+        """GET /inscricao/ must return status_code 200"""
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template(self):
+        """GET /inscricao/ must use template subscription/subscription_form.html"""
+        self.assertTemplateUsed(self.response, 'subscription/subscription_form.html')
+
+    def test_html(self):
+        """HTML must contain input tags"""
+        self.assertContains(self.response, '<form')
+        self.assertContains(self.response, '<input', 6)
+        self.assertContains(self.response, 'type="text"', 3)
+        self.assertContains(self.response, 'type="email"')
+        self.assertContains(self.response, 'type="submit"')
+
+    def test_csrf(self):
+        """HTML must contain csrf_token"""
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_has_form(self):
+        """Context must have SubscriptionForm"""
+        form = self.response.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_fields(self):
+        """Form must have 4 fields"""
+        form = self.response.context['form']
+        self.assertSequenceEqual(['name','cpf','phone','email'], list(form.fields))
+
