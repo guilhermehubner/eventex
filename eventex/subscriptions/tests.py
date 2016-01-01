bc8bc3c2742be29bb58556405424754156a50946
Guilherme <guilherme_hubner@msn.com>
2016-01-01 18:06:17 -0200

    Realiza inscrição

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,3 +1,4 @@
+from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 
@@ -35,3 +36,75 @@ class SubscriptionsTests(TestCase):
         """Form must have 4 fields"""
         form = self.response.context['form']
         self.assertSequenceEqual(['name', 'cpf', 'phone', 'email'], list(form.fields))
+
+
+class SubscriptionsPostTests(TestCase):
+    def setUp(self):
+        data = {'name': 'Guilherme Hubner', 'cpf': '12345678901', 'phone': '31-99223847',
+                'email': 'guilherme_hubner@msn.com'}
+        self.response = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        """Valid POST should redirect to /inscricao/"""
+        self.assertEqual(self.response.status_code, 302)
+
+    def test_send_subscribe_email(self):
+        """Valid POST should send one email"""
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_subscription_email_subject(self):
+        """Subscription email subject must be 'Confirmação de Inscrição'"""
+        email = mail.outbox[0]
+        expect = 'Confirmação de Inscrição'
+        self.assertEqual(email.subject, expect)
+
+    def test_subscription_email_from(self):
+        """Subscription email from must be 'contato@eventex.com.br'"""
+        email = mail.outbox[0]
+        expect = 'contato@eventex.com.br'
+        self.assertEqual(email.from_email, expect)
+
+    def test_subscription_email_to(self):
+        """Subscription email to must be 'guilherme_hubner@msn.com'"""
+        email = mail.outbox[0]
+        expect = ['contato@eventex.com.br', 'guilherme_hubner@msn.com']
+        self.assertEqual(email.to, expect)
+
+    def test_subscription_email_body(self):
+        """Subscription email body must have POST data"""
+        email = mail.outbox[0]
+        self.assertIn('Guilherme Hubner', email.body)
+        self.assertIn('12345678901', email.body)
+        self.assertIn('guilherme_hubner@msn.com', email.body)
+        self.assertIn('31-99223847', email.body)
+
+class SubscriptionsInvalidPost(TestCase):
+    def setUp(self):
+        self.response = self.client.post('/inscricao/', {})
+
+    def test_post(self):
+        """Invalid POST should not redirect"""
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template(self):
+         """Invalid POST should use template subscription/subscription_form.html"""
+         self.assertTemplateUsed(self.response, 'subscription/subscription_form.html')
+
+    def test_has_form(self):
+        """Context must have SubscriptionForm"""
+        form = self.response.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_errors(self):
+        """Form must have errors messages"""
+        form = self.response.context['form']
+        self.assertTrue(form.errors)
+
+class SubscriptionsSuccessMessage(TestCase):
+    def setUp(self):
+        data = {'name': 'Guilherme Hubner', 'cpf': '12345678901', 'phone': '31-99223847',
+                'email': 'guilherme_hubner@msn.com'}
+        self.response = self.client.post('/inscricao/', data, follow=True)
+
+    def test_message(self):
+        self.assertContains(self.response, 'Inscrição realizada com sucesso!')
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,7 +1,27 @@
+from django.contrib import messages
+from django.core import mail
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscription(request):
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+
+        if form.is_valid():
+            mail.send_mail('Confirmação de Inscrição',
+                           render_to_string('subscription/subscription_email.txt', form.cleaned_data),
+                           'contato@eventex.com.br',
+                           ['contato@eventex.com.br', form.cleaned_data['email']])
+
+            messages.success(request, 'Inscrição realizada com sucesso!')
+
+            return HttpResponseRedirect('/inscricao/')
+        else:
+            return render(request, 'subscription/subscription_form.html',
+                          {'form': form})
+
     context = {'form': SubscriptionForm()}
     return render(request, 'subscription/subscription_form.html', context)
