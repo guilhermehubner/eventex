85f21cc7d1b72d5192dcb4189a6c6fbb4e0e8fdd
Guilherme <guilherme_hubner@msn.com>
2016-02-09 15:38:53 -0200

    Keynote speakers

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -17,3 +17,16 @@ class HomeTest(TestCase):
     def test_subscription_link(self):
         expected = 'href="{}"'.format(resolve_url('subscriptions:new'))
         self.assertContains(self.response, expected)
+
+    def test_speakers(self):
+        """Must show keynote speakers"""
+        contents = ['Grace Hopper', 'http://hbn.link/hopper-pic',
+                    'Alan Turing', 'http://hbn.link/turing-pic']
+
+        for content in contents:
+            with self.subTest():
+                self.assertContains(self.response, content)
+
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(resolve_url('home'))
+        self.assertContains(self.response, expected)
--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,24 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from eventex.subscriptions.models import Subscription
-from eventex.subscriptions.validators import validade_cpf
-
-
-class SubscriptionFormOld(forms.Form):
-    name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF', validators=[validade_cpf])
-    phone = forms.CharField(label='Telefone', required=False)
-    email = forms.EmailField(label='Email', required=False)
-
-    def clean_name(self):
-        name = self.cleaned_data['name']
-        return ' '.join([w.capitalize() for w in name.split(' ')])
-
-    def clean(self):
-        if not self.cleaned_data.get('phone') and not self.cleaned_data.get('email'):
-            raise ValidationError('Informe seu e-mail ou telefone.')
-
-        return self.cleaned_data
 
 
 class SubscriptionForm(forms.ModelForm):
